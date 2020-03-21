d4aa003f129a2ce4b7cacc010531a741e9970eef
Alex Goodwin <cyberia.resurrection@gmail.com>
2020-03-22 02:16:04 +1000

    Implement trivial-rule detection.
    A rule is trivially true iff it contains both senses of a literal - such rules
    can be dropped from the rule set before it gets passed to the solver.

--- src/Composer/DependencyResolver/Preprocess/PreprocessRule.php
+++ src/Composer/DependencyResolver/Preprocess/PreprocessRule.php
@@ -12,5 +12,55 @@ use Composer\DependencyResolver\GenericRule;
 
 class PreprocessRule extends GenericRule
 {
+    const LITERAL_MODULUS = 32;
 
+
+    private $positiveLiteralHash = 0;
+    private $negativeLiteralHash = 0;
+    private $posLiterals = array();
+    private $negLiterals = array();
+
+    public function __construct(array $literals, $reason, $reasonData, array $job = null)
+    {
+        parent::__construct($literals, $reason, $reasonData, $job);
+
+        $this->posLiterals = array_filter($literals, function ($value) { return 0 < $value; });
+        $this->negLiterals = array_filter($literals, function ($value) { return 0 > $value; });
+        array_walk($this->negLiterals, function (&$value, $key) { $value = abs($value);});
+        $this->positiveLiteralHash = $this->literalHash($this->posLiterals);
+        $this->negativeLiteralHash = $this->literalHash($this->negLiterals);
+    }
+
+    /**
+     * Indicate whether rule is trivially true.
+     *
+     * @return bool
+     */
+    public function isTrivial()
+    {
+        if (0 == ($this->positiveLiteralHash & $this->negativeLiteralHash)) {
+            return false;
+        }
+
+        return 0 < count(array_intersect($this->posLiterals, $this->negLiterals));
+    }
+
+    private function literalHash(array $literals)
+    {
+        array_walk(
+            $literals,
+            function (&$value, $key) { $value = (abs($value) % PreprocessRule::LITERAL_MODULUS);}
+        );
+
+        $literals = array_unique($literals);
+
+        $result = 0;
+
+        foreach ($literals as $lit) {
+            $scale = 1 << $lit;
+            $result = $result | $scale;
+        }
+
+        return $result;
+    }
 }
---
+++ tests/Composer/Test/DependencyResolver/Preprocess/PreprocessRuleTest.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 22/03/20
+ * Time: 1:42 AM
+ */
+
+namespace Composer\Test\DependencyResolver\Preprocess;
+
+use Composer\DependencyResolver\Preprocess\PreprocessRule;
+use Composer\DependencyResolver\Rule;
+use Composer\Test\TestCase;
+
+class PreprocessRuleTest extends TestCase
+{
+    public function testRuleIsTrivial()
+    {
+        $rule = new PreprocessRule(array(42, -42), Rule::RULE_PACKAGE_REQUIRES, null);
+
+        $this->assertTrue($rule->isTrivial());
+    }
+
+    public function testRuleIsNotTrivialHashNoCollision()
+    {
+        $rule = new PreprocessRule(array(42, -41), Rule::RULE_PACKAGE_REQUIRES, null);
+
+        $this->assertFalse($rule->isTrivial());
+    }
+
+    public function testRuleIsNotTrivialHashCollision()
+    {
+        $rule = new PreprocessRule(array(42, -74), Rule::RULE_PACKAGE_REQUIRES, null);
+
+        $this->assertFalse($rule->isTrivial());
+    }
+}
