869bdb510f231688349c3de8793d15254d59bc36
Alex Goodwin <cyberia.resurrection@gmail.com>
2020-03-22 01:40:09 +1000

    Add skeleton of PreprocessRule.
    I didn't want to clutter up the existing, battle-tested, Rule classes
    with the bits that will be needed to support rule set preprocessing.

---
+++ src/Composer/DependencyResolver/Preprocess/PreprocessRule.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 22/03/20
+ * Time: 1:35 AM
+ */
+
+namespace Composer\DependencyResolver\Preprocess;
+
+use Composer\DependencyResolver\GenericRule;
+
+class PreprocessRule extends GenericRule
+{
+
+}
